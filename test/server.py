80684e481fa4854aade1f6354b86b3623a6bad81
lucas <lffsantos@gmail.com>
2015-12-12 12:21:37 -0300

    added accept again after send msg to client

--- test/server.py
+++ test/server.py
@@ -4,12 +4,16 @@ import socket
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
-s.listen(1)
+s.listen(2)
+print('wait')
 conn, addr = s.accept()
 print('Connected by', addr)
 while True:
     data = conn.recv(1024)
     if data:
         conn.sendall(b'lucas')
-# conn.close()
+        conn, addr = s.accept()
+        print("conectou")
+conn.close()
